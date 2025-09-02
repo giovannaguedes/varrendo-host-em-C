aa624ff6b0119d88b2b5b430ec5f00e1794e4ca8
giovannaguedes <154679630+giovannaguedes@users.noreply.github.com>
2025-09-02 15:20:13 -0300

    programa.c

---
+++ programa.c
@@ -0,0 +1,15 @@
+//Meu Primeiro programa em C
+#include <stdio.h>
+int main (void){
+
+	int porta = 80;
+	char ip[] = "192.168.0.1";
+	float ver = 1.1;
+	
+	printf ("Desec Security\n");
+	printf("Scan Versao: %f \n",ver);
+	printf("Varrendo Host: %s na porta %i \n", ip, porta);
+
+return 0;
+
+｝
